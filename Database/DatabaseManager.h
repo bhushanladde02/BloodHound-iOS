fa9f05363f8ac9f4c47253c7c2da2cc40b41c3d6
“pradeephrish” <chaudharipradeep9@gmail.com>
2014-08-27 14:15:46 -0700

    FMDB pod installed

---
+++ Database/DatabaseManager.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.h
+//  BloodHound
+//
+//  Created by Pradeep Chaudhari on 8/27/14.
+//  Copyright (c) 2014 Small Emperor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseManager : NSObject
+
+@end
---
+++ Database/DatabaseManager.m
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.m
+//  BloodHound
+//
+//  Created by Pradeep Chaudhari on 8/27/14.
+//  Copyright (c) 2014 Small Emperor. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+
+@implementation DatabaseManager
+
+@end
